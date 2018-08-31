782cd96c63bcb37901018c815805a8f2ae42948f
murari <murari@emproto.com>
2018-08-31 11:49:01 +0530

    Balance inquiry implemented.

--- src/main/java/com/eot/api/TransactionController.java
+++ src/main/java/com/eot/api/TransactionController.java
@@ -15,8 +15,10 @@ import com.eot.core.LoginTypes;
 import com.eot.core.TransactionTypes;
 import com.eot.domain.dao.LoginDao;
 import com.eot.domain.model.Agent;
+import com.eot.domain.model.Customer;
 import com.eot.domain.model.Transaction;
 import com.eot.domain.services.AgentService;
+import com.eot.domain.services.CustomerService;
 import com.eot.domain.services.TransactionService;
 import com.eot.util.EotException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -30,6 +32,9 @@ public class TransactionController {
 
 	@Autowired
 	AgentService agentService;
+
+	@Autowired
+	CustomerService customerService;
 
 	@RequestMapping(value = "/api/transaction/{agentId}", method = RequestMethod.POST)
 	public ResponseEntity<Object> deposite(@PathVariable("agentId") String agentId,
@@ -47,16 +52,32 @@ public class TransactionController {
 		}
 
 	}
-	
+
 	@RequestMapping(value = "/api/miniStatement/{agentId}", method = RequestMethod.POST)
 	public ResponseEntity<Object> miniStatement(@PathVariable("agentId") String agentId,
 			@RequestBody Transaction transaction) {
 
 		try {
 
-		List<Transaction> list = transactionService.miniStatement(agentId, transaction);
+			List<Transaction> list = transactionService.miniStatement(agentId, transaction);
 			return ResponseEntity.status(HttpStatus.OK).body(list);
 
+		}
+
+		catch (EotException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new String(e.getMessage()));
+		}
+
+	}
+
+	@RequestMapping(value = "/api/balanceEnquiry/{agentId}", method = RequestMethod.POST)
+	public ResponseEntity<Object> balanceEnquiry(@PathVariable("agentId") String agentId,
+			@RequestBody Transaction transaction) {
+
+		try {
+
+			Double customerBalance = transactionService.balanceEnquiry(transaction.getAccountNumber());
+			return ResponseEntity.status(HttpStatus.OK).body(customerBalance);
 		}
 
 		catch (EotException e) {
--- src/main/java/com/eot/domain/services/TransactionService.java
+++ src/main/java/com/eot/domain/services/TransactionService.java
@@ -6,9 +6,11 @@ import com.eot.domain.model.Transaction;
 import com.eot.util.EotException;
 
 public interface TransactionService {
-	
-	public void deposite(String agentId,Transaction transaction) throws EotException;
-	
-	public List<Transaction> miniStatement(String agentId,Transaction transaction) throws EotException;
+
+	public void deposite(String agentId, Transaction transaction) throws EotException;
+
+	public List<Transaction> miniStatement(String agentId, Transaction transaction) throws EotException;
+
+	public Double balanceEnquiry(String accountNumber) throws EotException;;
 
 }
